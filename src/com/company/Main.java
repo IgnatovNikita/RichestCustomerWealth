3544e7aea0118131e3d1be9f401688b8267fbcd9
IgnatovNikita <star08409@gmail.com>
2022-07-27 22:33:51 +0300

    Решенная задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class Main {
 
+public class Main {
+    public static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] account : accounts) {
+            int sum = account[0];
+            for (int j = 1; j < accounts[0].length; j++) {
+                sum += account[j];
+            }
+            max = Math.max(max, sum);
+        }
+
+
+        return max;
+    }
     public static void main(String[] args) {
 	// write your code here
+        int [][] s = {{1,2,3},{3,2,1}};
+        int [][] s1 = {{1,5},{7,3},{3,5}};
+        System.out.println(maximumWealth(s));
+        System.out.println(maximumWealth(s1));
     }
 }
